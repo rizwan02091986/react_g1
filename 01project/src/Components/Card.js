df7eabf8496b970b370f454faf3ff2b0f5c3b1ff
M.Rizwan <rizwan02091986@gmail.com>
2024-05-23 20:39:49 +0500

    23-05-2024

---
+++ 01project/src/Components/Card.js
@@ -0,0 +1,28 @@
+function Card(){
+
+    
+    return(
+        <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/23623515/pexels-photo-23623515/free-photo-of-a-building-in-madrid.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width="384" height="512"/>
+    <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+      <blockquote>
+        <p class="text-lg font-medium">
+          “Tailwind CSS is the only framework that I've seen scale
+          on large teams. It’s easy to customize, adapts to any design,
+          and the build size is tiny.”
+        </p>
+      </blockquote>
+      <figcaption class="font-medium">
+        <div class="text-sky-500 dark:text-sky-400">
+          Sarah Dayan
+        </div>
+        <div class="text-slate-700 dark:text-slate-500">
+          Staff Engineer, Algolia
+        </div>
+      </figcaption>
+    </div>
+  </figure>
+    );
+}
+
+export default Card;
