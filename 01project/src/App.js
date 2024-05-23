dc2887130cfecf00673936e626a7e75850988f1b
M.Rizwan <rizwan02091986@gmail.com>
2024-05-23 19:45:48 +0500

    23-05-2024

---
+++ 01project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <h1>Hello React</h1>
+  <h1>Hello React</h1>
+  <h1>Hello React</h1>
+  <h1>Hello React</h1>
+  <h1>Hello React</h1>
+  </>
+
+  );
+}
+
+export default App;
