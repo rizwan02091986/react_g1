832a808dc9106e1f8d43f2bedf7f4825daa776ea
M.Rizwan <rizwan02091986@gmail.com>
2024-05-23 20:23:28 +0500

    23-05-2024

--- 01project/src/App.js
+++ 01project/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
   <>
-  <h1>Hello React</h1>
+  <h1 className= 'bg-red-950 text-white text-center text-6xl'>Hello React</h1>
   <h1>Hello React</h1>
   <h1>Hello React</h1>
   <h1>Hello React</h1>
